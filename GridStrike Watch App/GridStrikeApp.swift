d6fdf3217fa17963c5200d2237df0d65ba51b048
Philip Zvar <philip.zvar@gmail.com>
2026-05-03 22:36:03 +0200

    Initial Commit

---
+++ GridStrike Watch App/GridStrikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridStrikeApp.swift
+//  GridStrike Watch App
+//
+//  Created by Philip Zvar on 03/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct GridStrike_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GridStrike Watch AppTests/GridStrike_Watch_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  GridStrike_Watch_AppTests.swift
+//  GridStrike Watch AppTests
+//
+//  Created by Philip Zvar on 03/05/2026.
+//
+
+import Testing
+@testable import GridStrike_Watch_App
+
+struct GridStrike_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
